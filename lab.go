248150e8b52bcfd9e261beb9c9d7748e4e0acef4
arlsclu <arlsclu@yahoo.com>
2020-04-04 11:45:09 +0800

    unix init

--- lab.go
+++ lab.go
@@ -3,9 +3,26 @@ package main
 import (
 	"fmt"
 
-	"github.com/arlsclu7/golab/morestrings"
+	"github.com/arlsclu7/golab/types"
 )
 
+func newInt() *int {
+	return new(int)
+}
 func main() {
-	fmt.Println(morestrings.ReverseRunes("abc123"))
+
+	fmt.Println(types.P1())
+
+	for i := 1; i < 10; i++ {
+		fmt.Printf("%x \n", i)
+	}
+
+	ptr := new(float32)
+	a := "mystring"
+	b := &a
+	fmt.Println(b)
+	fmt.Println("ptr address", b)
+	fmt.Println("ptr value", *b)
+	fmt.Println("b adress", ptr)
+	fmt.Println("b value", *ptr)
 }
---
+++ types/base.go
@@ -0,0 +1,12 @@
+package types
+
+var arr [3]int = [3]int{1, 2, 3}
+
+const a = 100
+
+/*
+P1 FEEE
+*/
+func P1() rune {
+	return '平'
+}
