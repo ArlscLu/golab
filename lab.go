8fc6eb72772751155392b149c457a05fc5ee5209
arlsclu <arlsclu@yahoo.com>
2020-04-08 15:30:23 +0800

    omdify]

--- lab.go
+++ lab.go
@@ -7,6 +7,18 @@ import (
 )
 
 func main() {
+
+	var a = 1
+	a = a << 8
+
+	fmt.Printf("%b", a)
+	fmt.Printf("%d", a-1)
+
+	fmt.Println()
+	var b = 10
+	var c = &b
+	fmt.Println(c)
+
 	fmt.Println(types.B)
 	fmt.Println(types.Num)
 	fmt.Printf("%d \n", types.Num)
@@ -14,23 +26,5 @@ func main() {
 	fmt.Printf("%o \n", types.Num)
 	fmt.Printf("%x,%X \n", types.Num, types.Num)
 	fmt.Printf("%x\n", types.X)
-	fmt.Printf("%U", "上海")
-	fmt.Print("111111\n")
-	a := "中国"
-	fmt.Println(len(a))
-	for i := 0; i < len(a); i++ {
-		fmt.Printf("%x ", a[i])
-	}
-	fmt.Println()
-	for i := 0; i < len(a); i++ {
-		fmt.Printf("%c ", a[i])
-	}
-	s := "中国"
-	r := []rune(s)
-	for i := 0; i < len(r); i++ {
-		fmt.Printf("%x", r[i])
-	}
-	for i := 0; i < len(r); i++ {
-		fmt.Printf("%c", r[i])
-	}
+
 }
--- types/base.go
+++ types/base.go
@@ -8,5 +8,3 @@ var Num int = 13
 
 //X string
 var X string = "U+000A"
-
-var Y string = "上海"
