b836befb7edcc9c7daf2d2b3d9a4cc6b01af1336
arlsclu <arlsclu@yahoo.com>
2021-04-26 17:08:35 +0800

    modify varname

--- tgorm/init.go
+++ tgorm/init.go
@@ -10,6 +10,7 @@ var db *gorm.DB
 func init() {
 	// dsn := "test_minqijia:test_minqijia_rw@tcp(10.20.1.20:3306)/towngas_community?charset=utf8mb4&parseTime=True&loc=Local"
 	dsn := "go:!tewe&213QQ@tcp(127.0.0.1:3306)/blog"
+	dsn = "root:!12345Qwert@tcp(127.0.0.1:3306)/blog"
 	gormDb, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
